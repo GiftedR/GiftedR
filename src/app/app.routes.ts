affb8317016e0cc6491eed855cb35d98d900ffb7
GiftedR <kidsgamers@hotmail.com>
2024-12-03 23:46:02 -0800

    Created Experience, Home, Navbar, and Projects page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ExperienceComponent } from './experience/experience.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{ path: '', component: HomeComponent },
+	{ path: 'projects', component: ProjectsComponent},
+	{ path: 'experience', component: ExperienceComponent}
+];
